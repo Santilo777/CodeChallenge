7a1121aeb49ccbe64633930144a01d049c681ca6
Santiago Lopez <santiagolopezn7@gmail.com>
2023-09-04 12:17:37 -0300

    Se corrige y agrega estilo

--- Code Challenge/script.js
+++ Code Challenge/script.js
@@ -1,35 +1,20 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const formulario = document.getElementById("registroForm");
-    const enviarBtn = document.getElementById("enviarBtn");
-    const respuesta = document.getElementById("respuesta");
+document.getElementById("formulario").addEventListener("submit", function(event) {
+    event.preventDefault();
 
-    enviarBtn.addEventListener("click", function () {
-        // Obtener los valores del formulario
-        const nombre = document.getElementById("nombre").value;
-        const apellido = document.getElementById("apellido").value;
-        const fechaNacimiento = document.getElementById("fechaNacimiento").value;
+    const data = {
+        nombre: document.getElementById("nombre").value,
+        apellido: document.getElementById("apellido").value,
+        fechaNacimiento: document.getElementById("fechaNacimiento").value
+    };
 
-        // Crear un objeto con los datos del formulario
-        const datos = {
-            nombre: nombre,
-            apellido: apellido,
-            fechaNacimiento: fechaNacimiento
-        };
-
-        // Realizar la solicitud POST utilizando fetch
-        fetch("https://jsonplaceholder.typicode.com/users", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(datos)
-        })
-        .then(response => response.json())
-        .then(data => {
-            respuesta.innerHTML = "Respuesta del servidor: " + JSON.stringify(data);
-        })
-        .catch(error => {
-            respuesta.innerHTML = "Error al enviar la solicitud: " + error;
-        });
-    });
+    fetch('https://jsonplaceholder.typicode.com/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
 });
